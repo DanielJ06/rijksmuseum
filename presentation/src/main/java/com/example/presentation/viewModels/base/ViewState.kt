5a9f2ac44079884e89d09c9e058a517975046435
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-02 11:02:01 -0300

    create base ViewState class

---
+++ presentation/src/main/java/com/example/presentation/viewModels/base/ViewState.kt
@@ -0,0 +1,12 @@
+package com.example.presentation.viewModels.base
+
+sealed class ViewState<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<T>(data: T): ViewState<T>(data)
+    class Error<T>(message: String?, data: T? = null): ViewState<T>(data, message)
+    class Loading<T>: ViewState<T>()
+
+}
