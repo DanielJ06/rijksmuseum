25a006b644a71e3583ed9d97711918a2039468c8
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-02 11:09:09 -0300

    Paint Service interface

---
+++ data_remote/src/main/java/com/example/data_remote/services/PaintsService.kt
@@ -0,0 +1,14 @@
+package com.example.data_remote.services
+
+import com.example.data_remote.response.PaintResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PaintsService {
+
+    @GET("collection?imgonly")
+    suspend fun loadPaints(
+        @Query("page") page: Int
+    ): PaintResponse
+
+}
