81ea970082e398de4377ed2c96603c520fa5c1e9
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-02 10:57:51 -0300

    Create BaseApplication and startKoin

--- app/src/main/java/com/example/rijksmuseum/BaseApplication.kt
+++ app/src/main/java/com/example/rijksmuseum/BaseApplication.kt
@@ -1,12 +1,36 @@
 package com.example.rijksmuseum
 
 import android.app.Application
+import com.example.di.*
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class BaseApplication : Application() {
 
+    companion object {
+        lateinit var instance: BaseApplication
+    }
+
+    init {
+        instance = this
+    }
+
     override fun onCreate() {
         super.onCreate()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidContext(this@BaseApplication)
+            modules(
+                dataSourceModule,
+                domainModule,
+                presentationModule,
+                repositoryModule,
+                serviceModule
+            )
+        }
     }
 
 }
