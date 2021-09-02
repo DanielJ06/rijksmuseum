5010244fc274304f0d1c331005e5d10814989913
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-02 11:00:47 -0300

    add gson as ConverterFactory

--- data_remote/src/main/java/com/example/data_remote/base/WebServiceFactory.kt
+++ data_remote/src/main/java/com/example/data_remote/base/WebServiceFactory.kt
@@ -1,15 +1,18 @@
 package com.example.data_remote.base
 
 import com.example.data_remote.utils.ApiKeyInterceptor
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 inline fun <reified T> createWebService(baseUrl: String, okHttpClient: OkHttpClient): T =
     Retrofit.Builder()
         .baseUrl(baseUrl)
         .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
         .build()
         .create(T::class.java)
 
@@ -19,7 +22,9 @@ fun getOkHttpClient(): OkHttpClient {
 
     return OkHttpClient.Builder()
         .addInterceptor(ApiKeyInterceptor())
-        .addInterceptor(loggingInterceptor)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
         .connectTimeout(45, TimeUnit.SECONDS)
         .callTimeout(45, TimeUnit.SECONDS)
         .build()
