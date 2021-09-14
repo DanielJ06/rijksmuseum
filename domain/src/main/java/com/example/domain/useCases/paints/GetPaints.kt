24a609f3b9f76e15c1668a1321bc78e53491f517
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-14 10:47:30 -0300

    add cache options, and allow params to be null

--- domain/src/main/java/com/example/domain/useCases/paints/GetPaints.kt
+++ domain/src/main/java/com/example/domain/useCases/paints/GetPaints.kt
@@ -1,24 +1,25 @@
 package com.example.domain.useCases.paints
 
+import android.util.Log
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.domain.models.PaintProps
 import com.example.domain.repository.PaintRepository
 import com.example.domain.utils.UseCase
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 
 class GetPaints(
     scope: CoroutineScope,
     private val paintRepository: PaintRepository
 ) : UseCase<PagingData<PaintProps>, GetPaints.Params>(scope) {
 
-    override suspend fun run(params: Params?) = when (params) {
-        null -> throw Exception("Params cannot be null")
-        else -> try {
-            paintRepository.getPaintsStream()
+    override suspend fun run(params: Params?): Flow<PagingData<PaintProps>> {
+        try {
+            return paintRepository.getPaintsStream().cachedIn(scope)
         } catch (e: Exception) {
             throw e
         }
-
     }
 
     data class Params(
