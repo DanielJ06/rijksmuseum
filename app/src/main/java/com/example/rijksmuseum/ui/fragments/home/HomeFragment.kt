d2cc744b63d7539342a7ef4e274d7198eaa17ab0
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-09-14 10:49:52 -0300

    setup paging 3 adapter, handle exceptions, and use grid layout instead linear

--- app/src/main/java/com/example/rijksmuseum/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/rijksmuseum/ui/fragments/home/HomeFragment.kt
@@ -1,22 +1,21 @@
 package com.example.rijksmuseum.ui.fragments.home
 
 import android.os.Bundle
+import android.transition.ChangeBounds
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.base_feature.core.BaseFragment
 import com.example.base_feature.utils.delegateProps.viewInflateBinding
 import com.example.presentation.viewModels.PaintViewModel
 import com.example.presentation.viewModels.base.ViewState
 import com.example.rijksmuseum.databinding.FragmentHomeBinding
 import com.example.rijksmuseum.ui.adapters.PaintItemAdapter
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -35,23 +34,50 @@ class HomeFragment : BaseFragment() {
 
     override fun setupView() {
         mViewModel.loadPaints()
-
         binding.paintingsRv.adapter = mAdapter
-        val linearLayout = LinearLayoutManager(
-            requireContext(), LinearLayoutManager.VERTICAL, false
+        val gridLayout = GridLayoutManager(
+            requireContext(), 2, GridLayoutManager.VERTICAL, false
         )
-        binding.paintingsRv.layoutManager = linearLayout
-
-        initViewModel()
+        binding.paintingsRv.layoutManager = gridLayout
     }
 
-    private fun initViewModel() {
-        lifecycleScope.launch {
-            mViewModel.loadPaints().collectLatest {
-                Log.i("DEBUG", it.toString())
-                mAdapter.submitData(it)
+    override fun addObservers(owner: LifecycleOwner) {
+        mAdapter.addLoadStateListener { loadState ->
+            if (loadState.refresh is LoadState.Loading) {
+                binding.loadingAnimation.visibility = View.VISIBLE
+            } else if (loadState.refresh is LoadState.NotLoading) {
+                binding.loadingAnimation.visibility = View.INVISIBLE
+            }
+
+            val errorState = when {
+                loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                else -> null
+            }
+            errorState?.let {
+                binding.errorText.visibility = View.VISIBLE
+                binding.errorText.text = it.error.message
             }
         }
+
+        mViewModel.paintsViewState.observe(owner, { res ->
+            when (res) {
+                is ViewState.Success -> {
+                    res.data?.let {
+                        lifecycleScope.launch {
+                            mAdapter.submitData(it)
+                        }
+                    }
+                }
+                is ViewState.Error -> {
+                    Log.i("DEBUG", res.message.toString())
+                }
+                else -> {
+                    Log.i("DEBUG", res.message.toString())
+                }
+            }
+        })
     }
 
 }
